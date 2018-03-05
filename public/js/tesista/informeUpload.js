263aa57a6dd3e536cd01e29e4685785cf804b530
carnadaxxx <w.coronadobarrantes@gmail.com>
2018-03-05 11:40:18 -0500

    empesando el upload

--- public/js/tesista/informeUpload.js
+++ public/js/tesista/informeUpload.js
@@ -1,11 +1,43 @@
 $(function () {
+
+    var files = $('#files');
+
     $('#fileupload').fileupload({
+
+        url: 'index.php?controller=Upload',
         dataType: 'json',
-        acceptFileTypes: '/(\.|\/)(pdf)$/i',
-        done: function (e, data) {
-            $.each(data.result.files, function (index, file) {
-                $('<p/>').text(file.name).appendTo(document.body);
-            });
+        autoUpload: false
+
+    }).on('fileuploadadd', function (e, data) {
+
+        var fileTypeAllowed = /.\.(pdf)$/i;
+
+        var fileName = data.originalFiles[0]['name'];
+
+        var fileType = data.originalFiles[0]['type'];
+
+        console.log(fileType + "  " + fileName);
+
+        if (!fileTypeAllowed.test(fileName)) {
+
+            $('#fileupload').addClass('is-invalid').after('<div class="invalid-tooltip">El archivo que as adjuntado no es *.pdf.</div>');
+            console.log("esto NOOOOOO es un pdf");
+
+        } else {
+
+            $('#fileupload').addClass('is-valid');
+            $('#fileupload').removeClass('is-invalid');
+
+            console.log("esto es un pdf");
+
         }
+
+    }).on('fileuploaddone', function (e, data) {
+
+
+
+    }).on('fileuploadprogressall', function(e, data) {
+
+
     });
 });
